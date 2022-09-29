38a187e8b15b9ea3f22e7e2c7ed8d3fe705af096
crag3r <62219471+crag3r@users.noreply.github.com>
2022-09-29 14:00:55 +0200

    29.09.2022 - 14:00

--- Python/22.09.2022/main.py
+++ Python/22.09.2022/main.py
@@ -3,18 +3,6 @@ import random
 
 s = turtle.Screen()
 
-def mright():
-    tutel.fd(15)
-
-def mleft():
-    tutel.back(15)
-
-def mdown():
-    
-
-def mup():
-
-
 obstacle1 = turtle.Turtle()
 obstacle1.penup()
 obstacle1.shape("square")
@@ -25,10 +13,41 @@ obstacle2.penup()
 obstacle2.shape("square")
 obstacle2.goto(random.randint(-300,300), random.randint(-300,300))
 
+obstacle1.onclick(lambda x,y: (obstacle1.hideturtle(), obstacle1.goto(58008, 58008)))
+obstacle2.onclick(lambda x,y: (obstacle2.hideturtle(), obstacle2.goto(58008, 58008)))
+
 tutel = turtle.Turtle()
 tutel.shape("turtle")
+tutel.penup()
 
-turtle.listen()
+def colide(tut, obs):
+    return abs(tut.xcor() - obs.xcor()) < 24 and abs(tut.ycor() - obs.ycor()) < 24
+
+def mright():
+    if colide(tutel, obstacle1) == True or colide(tutel,obstacle2) == True:
+        tutel.reset()
+        tutel.penup()
+    tutel.setx(tutel.xcor()+15)
+
+def mleft():
+    if colide(tutel, obstacle1) == True or colide(tutel,obstacle2) == True:
+        tutel.reset()
+        tutel.penup()
+    tutel.setx(tutel.xcor()-15)
+
+def mdown():
+    if colide(tutel, obstacle1) == True or colide(tutel,obstacle2) == True:
+        tutel.reset()
+        tutel.penup()
+    tutel.sety(tutel.ycor()-15)
+
+def mup():
+    if colide(tutel, obstacle1) == True or colide(tutel,obstacle2) == True:
+        tutel.reset()
+        tutel.penup()
+    tutel.sety(tutel.ycor()+15)
+
+s.listen()
 
 s.onkeypress(mright, "Right")
 s.onkeypress(mleft, "Left")
@@ -37,5 +56,4 @@ s.onkeypress(mdown, "Down")
 
 
 
-turtle.exitonclick()
-
+s.mainloop()

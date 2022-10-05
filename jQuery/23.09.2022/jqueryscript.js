294877ef73060b72a6da12e52fa2dffaa0733792
makapaka <pakamaka@gmail.com>
2022-10-05 09:14:49 +0200

    05.10.2022 - 9:14

--- jQuery/23.09.2022/jqueryscript.js
+++ jQuery/23.09.2022/jqueryscript.js
@@ -1,3 +1,15 @@
 $(document).ready(() => {
-    
+    $(".tictac").hide()
+
+    $("#opcje").change(function(){
+        $(".tictac").show()
+        $("#opcje").hide()
+        $("#labelel").hide()
+        mode = $("#opcje").val()
+        console.log(mode)
+    })
+
+    $("#reset").click(function() {
+        location.reload()
+    })
 })

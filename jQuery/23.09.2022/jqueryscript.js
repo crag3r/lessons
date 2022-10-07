a29f6c91ad28fca27bc89603ece858b03f028efc
makapaka <pakamaka@gmail.com>
2022-10-07 09:38:25 +0200

    07.10.2022 - 9:38

--- jQuery/23.09.2022/jqueryscript.js
+++ jQuery/23.09.2022/jqueryscript.js
@@ -12,4 +12,43 @@ $(document).ready(() => {
     $("#reset").click(function() {
         location.reload()
     })
+
+    function jakiZnak(num) {
+        if(num==2) {
+            return "O"
+        }
+        else if(num==1) {
+            return "X"
+        }
+    }
+
+
+    let znak = 1;
+    $("td").click(function() {
+        
+        if(mode=="pvp") {
+            if($(this).text()=='') {
+                $(this).text(jakiZnak(znak))
+                if (znak%2==0) {
+                    znak=1
+                }
+                else {
+                    znak=2
+                }
+            }
+            else {
+                $(this).css("animation", "nope .5s")
+                setTimeout(() => {
+                    $(this).css("animation", "none")
+                },500)
+            }
+        }
+        else if(mode=="pc") {
+            
+        }
+        else {
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        }
+
+    })
 })

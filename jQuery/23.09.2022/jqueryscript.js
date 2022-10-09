4f8a3a8f4153a9cb2198911320e861165a4b7fd1
crag3r <k.zawadzki.k@gmail.com>
2022-10-09 15:27:45 +0200

    09.10.2022 - 15:28

--- jQuery/23.09.2022/jqueryscript.js
+++ jQuery/23.09.2022/jqueryscript.js
@@ -6,8 +6,8 @@ $(document).ready(() => {
         $(".tictac").show()
         $("#opcje").hide()
         $("#labelel").hide()
-        mode = $("#opcje").val()
-        if (mode=="pvp") {
+        mode = $("#opcje").val() //ustawianie trybu gry
+        if (mode=="pvp") { //wyświetlanie innych informacji w zależności od trybu gry
             $("#info").show()
             $("#info").text("PLAYER 1 - X | PLAYER 2 - O")
         }
@@ -33,11 +33,11 @@ $(document).ready(() => {
 //---------------SPRAWDZANIE WYGRANYCH (Trochę spaghetti)--------------------
     function WinCheck(znak) {
         if (
-            $(".id1").text()==znak && 
+            $(".id1").text()==znak && //sprawdzanie jakie znaki stoją w danych kombinacjach
             $(".id2").text()==znak &&
             $(".id3").text()==znak
         ) {
-            $(".id1").css("background-color", "green")
+            $(".id1").css("background-color", "green") //Jeśli wygrał zmień wygrane pola na zielony
             $(".id2").css("background-color", "green")
             $(".id3").css("background-color", "green")
             return true
@@ -118,42 +118,47 @@ $(document).ready(() => {
     function random_choice(array) {
         return array[Math.floor(Math.random() * array.length)]
       }
-    let pola = ["id1","id2","id3","id4","id5", "id6", "id7", "id8", "id9"]
+    let pola = ["id1","id2","id3","id4","id5", "id6", "id7", "id8", "id9"] //dostępne pola
     $("td").click(function() {
         
-        if(mode=="pvp") {
-            if($(this).text()=='') {
-                $(this).text(jakiZnak(znak))
+        if(mode=="pvp") { //jeśli tryb gry to PVP
+            if($(this).text()=='') { //jeśli puste pole
+                $(this).text(jakiZnak(znak)) //wstawianie znaku
                 if(WinCheck(jakiZnak(znak))) {
-                    mode="endgame"
+                    mode="endgame" //ustawianie trybu na endgame aby nikt nie mógł nic wstawić
                 }
-                if (znak%2==0) {
+                if (znak%2==0) { //zmiana znaku
                     znak=1
                 }
                 else {
                     znak=2
                 }
             }
-            else {
+            else { //animacja zajętego pola
                 $(this).css("animation", "nope .5s")
                 setTimeout(() => {
                     $(this).css("animation", "none")
                 },500)
             }
         }
-        else if(mode=="pc") {
-            if($(this).text()=='') {
-                $(this).text("X")
-                if(WinCheck(jakiZnak(znak))) {
-                    mode="endgame"
+        else if(mode=="pc") { //jeśli tryb gry to gracz vs computer
+            if($(this).text()=='') { //jeśli puste pole
+                $(this).text("X") //wstawianie znaku
+                pola = pola.filter(function removeItem(item) { //funkcja która miała usuwać pola zajęte ale nie działa nie wiem czemu:(
+                    return item != $(this).attr('class')
+                })
+                if(WinCheck(jakiZnak(znak))) { //sprawdzanie czy wygrał po ruchu
+                    mode="endgame" //ustawianie trybu na endgame aby nikt nie mógł nic wstawić
                 }
-                console.log(random_choice(pola))
-                $(`.${random_choice(pola)}`).text("O")
-                if(WinCheck(jakiZnak(znak))) {
-                    mode="endgame"
+
+                if(mode=="pc") { //sprawdzanie czy poprzedni ruch nie zakończył gry
+                    $(`.${random_choice(pola)}`).text("O") //wstawianie przez computer w losowe pole
+                }
+                if(WinCheck("O")) { //sprawdzanie czy wygrał po ruchu
+                    mode="endgame" //ustawianie trybu na endgame aby nikt nie mógł nic wstawić
                 }
             }
-            else {
+            else { //animacja zajętego pola
                 $(this).css("animation", "nope .5s")
                 setTimeout(() => {
                     $(this).css("animation", "none")
